e99f289edc21450a94ef6cfbd3d3e350a586585e
Ny Sreynich <sreynichny220@gmail.com>
2024-12-22 20:54:43 +0700

    version1

---
+++ resources/views/Admin/dasboard_screen.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="{{ asset('/css/dashboard.css') }}">
+    <title>Document</title>
+</head>
+<body>
+    <div class="side-menu">
+        <div class="brand-name">
+          <img src="/Image/norton.png">
+        </div>
+            <ul>
+                <li>Dashboard</li>
+                <li>Department</li>
+                <li>Student</li>
+                <li>My Schedule</li>
+                <li>All Task</li>
+        </ul>
+        <div class="logout_btn">
+            <a>Log out <i class="fa-solid fa-right-from-bracket"></i></a>
+        </div>
+    </div>
+    <div class="container">
+        <div class="header">
+        </div>
+    </div>
+</body>
+</html>
