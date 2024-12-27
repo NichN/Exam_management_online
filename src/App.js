d9045e396709e95ff70128284f56273951f667a4
sengsokchan <gda758984@example.com>
2024-12-27 14:26:43 +0700

    a

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import StudentProfile from "./components/StudentProfile";
+
+function App() {
+    return (
+        <div className="App">
+            <StudentProfile />
+        </div>
+    );
+}
+
+export default App;
