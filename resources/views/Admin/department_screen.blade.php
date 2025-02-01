fa8e37ab5e3f3b3537226689c3756e39f8df7957
Nich <sreynichny220@gmail.com>
2025-02-01 16:35:36 +0700

    hello

--- resources/views/Admin/department_screen.blade.php
+++ resources/views/Admin/department_screen.blade.php
@@ -5,89 +5,65 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Department</title>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+
+    <!-- Bootstrap & Font Awesome -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" 
+            integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" 
+            crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
+    <!-- Custom CSS -->
     <link rel="stylesheet" href="{{ asset('/css/dashboard.css') }}">
-    <link rel="stylesheet" href="{{ asset('/css/department.css') }}">
+    <link rel="stylesheet" href="{{ asset('/css/department.css') }}"> 
+    
+    <!-- Custom JS -->
     <script src="{{ asset('/js/menu.js') }}"></script>
 </head>
 <body>
+
+<!-- Sidebar -->
 <div class="container">
-        @include('partials.admin_sidebar')
+    @include('partials.admin_sidebar')
 </div>
-<!-- <div class="side-menu">
-    <div class="logo">
-    <img src="/Image/norton.png" alt="Logo">
-    </div>
-    <ul class="menu-list">
-    <li class="menu-item" onclick="setActive(this)">
-        <a href="/admin/dashboard">
-            <i class="fas fa-home"></i> Dashboard
-        </a>        
-    </li>
-    <li class="menu-item" onclick="setActive(this)">
-        <a href="/admin/department">
-            <i class="fas fa-building"></i> Department
-        </a> 
-    </li>
-    <li class="menu-item" onclick="setActive(this)">
-        <a href="/admin/student">
-            <i class="fas fa-user-graduate"></i> Student
-        </a>    
-    </li>
-    <li class="menu-item" onclick="setActive(this)">
-        <a href="/admin/alltask">
-            <i class="fas fa-tasks"></i> All Task
-        </a>    
-    </li>
-    <li class="menu-item" onclick="setActive(this)">
-        <a href="/admin/myschedule">
-            <i class="fas fa-clock"></i> My Schedule
-        </a>
-    </li>
-</ul>
 
-<div class="logout">
-        <i class="fas fa-sign-out-alt"></i> Log out
-    </div>
-</div> -->
-
-<!-- <div class="container">
-    <div class="header">
-    <div class="header-icons">
-        <div class="notification">
-            <i class="fa fa-bell"></i>
-            <span class="notification-badge"></span>
-        </div>
-        <div class="user-profile">
-            <i class="fa fa-user-circle"></i>
-            <span class="user-name">Sun Nasy</span>
-        </div>
-    </div>
-    </div>
- </div> -->
-
- <div class="dashboard-container">
-    <h2>My Batch</h2>
-    <hr class="divider">
-    <div class="batch-container">
-        <div class="batch">
-            <h4>COM-24</h4>
-            <div class="batch-content">
-                <div class="labtop-icon">
-                    <i class="fas fa-laptop batch-icon"></i>
-                </div>
-                <a class="batch-major">
-                    <a href="/admin/department/detail">Computer Studies</a>
-                </a>
-            </div>
-            <div class="arrow-icon">
-                <i class="fas fa-arrow-right"></i>
-            </div>
-        </div>
+<!-- Main Content -->
+<div class="container text-center">
+    <h2 style="text-align: center" class="mt-3">My Batch</h2>
+    <hr class="divider mb-4">
+    <div class="row batch-container">
     </div>
 </div>
 
+<!-- Fetch and Display Departments -->
+<script>
+    document.addEventListener("DOMContentLoaded", function () {
+        fetch('http://127.0.0.1:8000/api/departments')
+            .then(response => response.json())
+            .then(data => {
+                const batchContainer = document.querySelector('.batch-container');
+                batchContainer.innerHTML = '';
+
+                data.forEach(department => {
+                    const batchHTML = `
+                        <div class="col-md-4 d-flex justify-content-center mb-4"> 
+                            <div class="card shadow-sm border-0">
+                                <div class="card-body">
+                                    <i class="fas fa-laptop laptop"></i>
+                                    <h5 class="card-title">${department.name.toUpperCase()}</h5>
+                                    <p class="card-text">${department.description}</p>
+                                    <a href="/admin/department/detail/${department.id}" class="btn btn-primary">
+                                        View Details <i class="fas fa-arrow-right"></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    `;
+                    batchContainer.innerHTML += batchHTML;
+                });
+            })
+            .catch(error => console.error('Error fetching departments:', error));
+    });
+</script>
 
 </body>
 </html>
